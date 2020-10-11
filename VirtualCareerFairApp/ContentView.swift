d43e57fab62ebfb5416f49e9efd18fdf8f12c828
Lilly Zhou <lillyzh@stanford.edu>
2020-10-11 00:00:52 -0700

    Initial Commit

---
+++ VirtualCareerFairApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VirtualCareerFairApp
+//
+//  Created by Lilly Zhou on 10/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VirtualCareerFairApp/VirtualCareerFairAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualCareerFairAppApp.swift
+//  VirtualCareerFairApp
+//
+//  Created by Lilly Zhou on 10/11/20.
+//
+
+import SwiftUI
+
+@main
+struct VirtualCareerFairAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
